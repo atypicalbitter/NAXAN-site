87b8da7f4f36c64864d25dcf7ff31e1693162117
Matt Dudman <mattdudman95@googlemail.com>
2024-10-27 09:54:29 +0000

    JS added to timeline section, and timeline section renamed. New images added but need to fix MQ and padding around this and the Meet The Team section.

--- app.js
+++ app.js
@@ -5,3 +5,24 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is.active');
   menuLinks.classList.toggle('active');
 }) 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const timelineSections = document.querySelectorAll('.timel-container');
+  const options = {
+    threshold: 0.9
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+  timelineSections.forEach(section => {
+    observer.observe(section);
+  });
+});

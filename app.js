94b7388ead5c3bde0aa445d997830b8fcd50bbf2
Matt Dudman <mattdudman95@googlemail.com>
2024-10-29 10:21:39 +0000

    Fade in effect added on page load and when transitioning between pages.

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+
+// -----------DOCUMENT FADE IN 
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".fade").classList.add("fade-in");
+});
+
+function handleLinkClick(event) {
+  event.preventDefault();
+  const link = event.target.href;
+
+
+  document.querySelector(".fade").classList.add("fade-out");
+
+
+  setTimeout(() => {
+    window.location.href = link;
+  }, 300); 
+}
+
+document.querySelectorAll("a").forEach((link) => {
+  if (link.getAttribute("href") !== "#") {
+    link.addEventListener("click", handleLinkClick);
+  }
+});
+
+
+
+// ----------- NAVBAR
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar-menu');
 
@@ -6,7 +34,7 @@ menu.addEventListener('click', function() {
   menuLinks.classList.toggle('active');
 }) 
 
-
+// ----------- TIMELINE
 document.addEventListener("DOMContentLoaded", function () {
   const timelineSections = document.querySelectorAll('.timel-container');
   const options = {
